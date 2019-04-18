c742c27bf1f094eb57801cb30e55cd159b7b98c8
Kaizen Castanos <kaizen.castanos@narrativ.com>
2019-04-18 16:46:49 -0400

    everything done besides similarity

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark import SparkContext, SparkConf
 
+from math import log
+
 os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3'
+
+def vectorize(total, arr):
+    res = [0 for i in range(total)]
+    for x in arr:
+        res[x[0]-1] = x[1]
+    return res
 
 if __name__ == "__main__":
 
@@ -16,14 +24,24 @@ if __name__ == "__main__":
 
     # collect textfile, turn it into an RDD
     text = sc.textFile('test_file.txt')
+    totalDocs = text.count()
     # split up the words per doc
     doc_to_line = text.map(lambda x: (x.split(" ")[0], (x.split(" ")[1:])))
+    # add document word count to each document
+    doc_to_line2 = doc_to_line.map(lambda kv: ((kv[0], len(kv[1])), kv[1]))
+    
     # create doc/word pairs and count 
-    pairs = doc_to_line.flatMap(lambda kv: (((kv[0], x), 1) for x in (kv[1])))\
+    pairs = doc_to_line2.flatMap(lambda kv: (((kv[0], x), 1) for x in (kv[1])))\
         .reduceByKey(lambda x, y: x+y)
-    # remap so that the doc is the key
-    count_map = pairs.map(lambda kv: (kv[0][0], [(kv[0][1], kv[1])]))
-    # reduce so that each doc now has an array of words and their count
-    pre_dataFrame = count_map.reduceByKey(lambda x, y: x+y).sortByKey()
-    for x in pre_dataFrame.collect():
-        print(x)
+    # at this stage: ((doc, length), word), word_count
+    new_pairs = pairs.map(lambda kv: (kv[0][0][0], (kv[0][1], kv[1], kv[0][0][1])))
+    pairs_with_tf = new_pairs.map(lambda x: (x[0], x[1] + ((x[1][1]/x[1][2]),) ))
+    
+    flip = pairs_with_tf.map(lambda x: (x[1][0], [(x[0],) + x[1][1:]])).reduceByKey(lambda x, y: x+y)
+    # Current state: (word, [doc, word_appearance, doc_length, tf])
+    pairs_with_idf = flip.map(lambda x: ((x[0], (log(totalDocs/len(x[1])))), x[1]))
+
+    pre_similarity = pairs_with_idf.map(lambda kv: (kv[0][0], [(int(x[0][3:]), x[3]*kv[0][1]) for x in kv[1]]))\
+        .map(lambda x: (x[0], vectorize(totalDocs, x[1])))
+    for i in pre_similarity.collect():
+        print(i)
